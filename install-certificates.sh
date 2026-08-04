2cf19b6f49c741e9aed710c3d6ec8c4566f4060f
Davide <keeshux@gmail.com>
2026-08-04 18:01:58 +0200

    Fix temporary keychain not set as default

--- install-certificates.sh
+++ install-certificates.sh
@@ -9,7 +9,6 @@ set -e
 
 certs_url="${CERTIFICATES_URL/https:\/\//https://${ACCESS_TOKEN}@}"
 certs_passphrase=$CERTIFICATES_PASSPHRASE
-certs_filename=certificates.zip
 tmp_root=$RUNNER_TEMP/certificates
 
 p12_path="$tmp_root/certificates.p12"
@@ -25,13 +24,18 @@ mkdir -p "$tmp_root"
 curl -O "$certs_url"
 unzip -P "$certs_passphrase" -d "$tmp_root" `basename $certs_url`
 
-# Certificates
+# Temporary keychain
 security create-keychain -p "$keychain_password" "$keychain_path"
-security set-keychain-settings -lut $keychain_timeout
+security set-keychain-settings -lut "$keychain_timeout" "$keychain_path"
 security unlock-keychain -p "$keychain_password" "$keychain_path"
+
+# Set default and list
+security default-keychain -d user -s "$keychain_path"
+security list-keychain -d user -s "$keychain_path"
+
+# Certificates
 security import "$p12_path" -P "$p12_password" -A -t cert -f pkcs12 -k "$keychain_path"
 security set-key-partition-list -S apple-tool:,apple: -k "$keychain_password" "$keychain_path"
-security list-keychain -d user -s "$keychain_path"
 
 # Provisioning
 mkdir -p "$profiles_root"
